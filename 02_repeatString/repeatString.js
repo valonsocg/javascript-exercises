9e576c734523a27474bbed35d934b3900406d149
valonsocg <valonsocg@gmail.com>
2024-07-16 12:28:05 -0500

    Resolve 1 and 2 exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function (text, number) {
+  if (number < 0) return "ERROR";
+  let txt = "";
+  for (let i = 1; i <= number; i++) {
+    txt += text;
+  }
+  return txt;
 };
 
 // Do not edit below this line
