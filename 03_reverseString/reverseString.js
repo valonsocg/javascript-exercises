03e377a5a52fe0b7354ac62abac831469ed6cd71
valonsocg <valonsocg@gmail.com>
2024-07-16 13:50:45 -0500

    Add third exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
+//Split, reverse, join
 
+const reverseString = function (str) {
+  const splittedStr = str.split("");
+  const reversedArray = splittedStr.reverse();
+  return reversedArray.join("");
 };
 
 // Do not edit below this line
