dc65373339a4d9f24760118d6a60439d8fced7da
valonsocg <valonsocg@gmail.com>
2024-07-16 20:54:05 -0500

    Finish exercise 7 tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+  const newTemp = (temp - 32) * (5 / 9);
+  return parseFloat(newTemp.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  const newTemp2 = temp * (9 / 5) + 32;
+  return parseFloat(newTemp2.toFixed(1));
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
