51354bb08b1b18ac9642eb1d095c5cb07cca7b87
valonsocg <valonsocg@gmail.com>
2024-07-16 19:48:33 -0500

    Finish exercise 5 sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function (start, end) {
+  if (
+    start < 0 ||
+    end < 0 ||
+    typeof start !== "number" ||
+    typeof end !== "number"
+  )
+    return "ERROR";
+  let sum = 0;
+  if (start < end) {
+    for (let i = start; i <= end; i++) {
+      sum += i;
+    }
+  } else {
+    for (let i = start; i >= end; i--) {
+      sum += i;
+    }
+  }
 
+  return sum;
 };
 
 // Do not edit below this line
