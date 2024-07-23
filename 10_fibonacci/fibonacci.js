ece56a1d31f0bc7d8de94be532911b299cbbde1f
valonsocg <valonsocg@gmail.com>
2024-07-22 21:21:46 -0500

    Complete exercise 10 fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+  if (num < 0) return "OOPS";
+  if (num === 0) return 0;
+  if (num === 1) return 1;
+  let fibArray = [0, 1];
+  for (let i = 2; i <= num; i++) {
+    fibArray[i] = fibArray[i - 1] + fibArray[i - 2];
+  }
+  return fibArray[num];
 };
 
 // Do not edit below this line
