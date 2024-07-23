c01cf4efce15764ae215ad487692e971e3acea00
valonsocg <valonsocg@gmail.com>
2024-07-22 21:04:12 -0500

    Complete exercise 09 palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  const cleanStr = str
+    .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()?\s]/g, "")
+    .toLowerCase();
+  const reverseStr = cleanStr.split("").reverse().join("");
+  return cleanStr === reverseStr;
 };
 
 // Do not edit below this line
