28dee76db151247329fae0a7aed21eeb11dc16e6
valonsocg <valonsocg@gmail.com>
2024-07-22 21:54:38 -0500

    Complete exercise 12 finTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function (arr) {
+  const currentYear = new Date().getFullYear();
 
+  const sortedArray = arr.sort((a, b) => {
+    const ageA = (a.yearOfDeath || currentYear) - a.yearOfBirth;
+    const ageB = (b.yearOfDeath || currentYear) - b.yearOfBirth;
+    if (ageA > ageB) {
+      return -1;
+    } else {
+      return 1;
+    }
+  });
+  return sortedArray[0];
 };
 
 // Do not edit below this line
